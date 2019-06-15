bdd14468d41bebed3321a69e50d74d86db8bc920
riccardozaffalon <riccardo.zaffalon@gmail.com>
2019-06-15 12:21:45 +0200

    Update smart panel service

--- run.sh
+++ run.sh
@@ -10,10 +10,4 @@ xset s off
 xset -dpms
 xset s noblank
 
-cd /apps/smart-panel/
-
-if ! lsof -i -P -n | grep :3000 >/dev/null ; then
-    node server.js &
-fi
-
 chromium-browser --no-sandbox http://localhost:3000 &
