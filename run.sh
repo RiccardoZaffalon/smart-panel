e6c4468ddd97536bd643c3faa95fe9b70b563213
riccardozaffalon <riccardo.zaffalon@gmail.com>
2019-06-15 12:02:27 +0200

    Update shell script to run commands in background mode

--- run.sh
+++ run.sh
@@ -1,15 +1,7 @@
 #!/bin/bash
 
-cd /apps/smart-panel/
-
-if ! lsof -i -P -n | grep :3000 >/dev/null ; then
-    node server.js &
-fi
-
-chromium-browser --no-sandbox http://localhost:3000
-
 # Turn TV on
-echo on 0 | cec-client -s -d 1
+echo on 0 | cec-client -s -d 1 &
 
 # Disable screensaver
 export DISPLAY=:0
@@ -17,3 +9,11 @@ xset q
 xset s off
 xset -dpms
 xset s noblank
+
+cd /apps/smart-panel/
+
+if ! lsof -i -P -n | grep :3000 >/dev/null ; then
+    node server.js &
+fi
+
+chromium-browser --no-sandbox http://localhost:3000 &
