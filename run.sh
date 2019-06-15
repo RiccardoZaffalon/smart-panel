7fcd551237aa3344b3b0d59634f3603babd21ce2
riccardozaffalon <riccardo.zaffalon@gmail.com>
2019-06-15 11:58:30 +0200

    Add no-sandbox to chromium

--- run.sh
+++ run.sh
@@ -6,7 +6,7 @@ if ! lsof -i -P -n | grep :3000 >/dev/null ; then
     node server.js &
 fi
 
-chromium-browser http://localhost:3000
+chromium-browser --no-sandbox http://localhost:3000
 
 # Turn TV on
 echo on 0 | cec-client -s -d 1
